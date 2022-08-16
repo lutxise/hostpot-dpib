723e02c2da3c853e6f6c9d7c8bd18fcf4a5a71a6
Lutxise <111191831+lutxise@users.noreply.github.com>
2022-08-16 14:04:41 +0700

    Add files via upload

---
+++ hostpot-dpib/js/custom.js
@@ -0,0 +1,9 @@
+/*==================================================================================
+    Custom JS (Any custom js code you want to apply should be defined here).
+====================================================================================*/
+
+// Carousel Settings Start
+$('.carousel').carousel({
+    interval: 3500
+  })
+// Carousel Settings End
